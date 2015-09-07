bc4ae3f489f8e32d532de37b241141ad1ea3c7ab
Quentin LAGARDE <quentin.lagarde@sopra.com>
2015-09-07 12:05:03 +0200

    ADD : Base file to prevent Jenkins build to fail

---
+++ MDKControl/MDKBase.m
@@ -0,0 +1,13 @@
+//
+//  MDKBase.m
+//  MDKControl
+//
+//  Created by Lagarde Quentin on 07/09/2015.
+//  Copyright (c) 2015 Sopra Steria. All rights reserved.
+//
+
+#import "MDKBase.h"
+
+@implementation MDKBase
+
+@end
